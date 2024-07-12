a6e4c861b4776d36c9b86122d39483f3d7b0cd6e
Muhammad <muhammadfabil40@gmail.com>
2024-07-12 10:16:25 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'loremflickr.com','picsum.photos'],
+    domains: ['localhost', 'loremflickr.com', 'picsum.photos'],
   },
   env: {
     NEXT_PUBLIC_BACKEND_URL: 'http://localhost:8080',
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig; // Correct way to export in a Node.js environment
